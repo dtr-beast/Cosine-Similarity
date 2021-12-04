7f7c9fee672248620193f596fb5538bc97121cb0
Aditya Sharma <mc5torkpro@gmail.com>
2021-12-04 16:47:17 +0530

    Added Responsiveness

--- pages/Form.tsx
+++ pages/Form.tsx
@@ -1,43 +1,6 @@
 import {FormEvent, useState} from "react";
-// import styled from "styled-components";
-// const randomSentence = require('random-sentence');
-// @ts-ignore
-// import randomSentence from 'random-sentence';
 import styles from '../styles/Button.module.css'
-import {IDFParams} from "./index";
-// interface CustomEventTarget extends EventTarget {
-//     query: HTMLInputElement
-//     document1: HTMLInputElement
-//     document2: HTMLInputElement
-//     document3: HTMLInputElement
-// }
-//
-// interface HTMLCustomForm extends FormEvent<HTMLFormElement> {
-//     target: CustomEventTarget
-// }
 
-// const Textarea = styled.textarea`
-//   //border: 1px black solid;
-//   //border-radius: 5px;
-//   //align-self: center;
-//   height: 200px;
-//   font-size: 1.2em;
-//   //width: 100%;
-//   padding: 15px;
-// `
-
-// const Button = styled.button`
-//   //border-radius: 5px;
-//   //margin: 10px;
-//   //padding: 0 10px;
-//   //background: blueviolet;
-//   //font-size: 2em;
-//   //text-align: center;
-// `
-
-// const label = styled.label`
-//   font-size: 2.5em;
-// `
 interface Params {
     onSubmit: (query: string, text1: string, text2: string, text3: string) => void
 }
@@ -54,15 +17,32 @@ export default function Form({onSubmit}: Params) {
     const [text3, setText3] = useState('')
 
     function fill() {
-        setQuery('gold silver truck')
-        setText1('Shipment of gold damaged in a fire')
-        setText2('Delivery of silver arrived in a silver truck')
-        setText3('Shipment of gold arrived in a truck')
+        const testCases =
+            [
+                [
+                    "gold silver truck",
+                    "Shipment of gold damaged in a fire",
+                    "Delivery of silver arrived in a silver truck",
+                    "Shipment of gold arrived in a truck"
+                ],
+                [
+                    "New York Times",
+                    "New York Past",
+                    "Los Angeles Past",
+                    "New New Times"
+                ]
+            ]
+        const randomTestCase = testCases[Math.floor(Math.random() * testCases.length)]
+        console.log(randomTestCase)
+        setQuery(randomTestCase[0])
+        setText1(randomTestCase[1])
+        setText2(randomTestCase[2])
+        setText3(randomTestCase[3])
     }
 
     return (
         <>
-            <form onSubmit={calculateSimilarity} className="container px-72 pt-24 mx-auto">
+            <form onSubmit={calculateSimilarity} className="container px-6 pt-2 lg:px-72 lg:pt-24 mx-auto">
                 <div className=" ">
                     <div className="mb-10">
                         <label className="dark:text-white font-bold text-3xl mr-4">Query*</label>
@@ -74,7 +54,7 @@ export default function Form({onSubmit}: Params) {
                             value={query} onChange={e => setQuery(e.target.value)} required/>
                     </div>
                 </div>
-                <div className="pt-24">
+                <div className="pt-4 lg:pt-24">
                     <div className="mb-10">
                         <label className="dark:text-white font-bold text-3xl mr-4">Document 1*</label>
                     </div>
@@ -84,7 +64,7 @@ export default function Form({onSubmit}: Params) {
                             value={text1} onChange={e => setText1(e.target.value)} required/>
                     </div>
                 </div>
-                <div className="pt-24">
+                <div className="pt-4 lg:pt-24">
                     <div className="mb-10">
                         <label className="dark:text-white font-bold text-3xl mr-4">Document 2</label>
                     </div>
@@ -94,7 +74,7 @@ export default function Form({onSubmit}: Params) {
                             value={text2} onChange={e => setText2(e.target.value)}/>
                     </div>
                 </div>
-                <div className="pt-24 pb-12">
+                <div className="pt-4 lg:pt-24 pb-12">
                     <div className="mb-10">
                         <label className="dark:text-white font-bold text-3xl mr-4">Document 3</label>
                     </div>
