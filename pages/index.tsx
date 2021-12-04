08dca757f594213842fed12e1d92d5c6ee36148c
Aditya Sharma <mc5torkpro@gmail.com>
2021-12-04 17:27:52 +0530

    Changed website to static by replacing <Image/> from next/image by a normal <img/> tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Form from "./Form";
 import React, {useState} from "react";
 import {useTheme} from 'next-themes'
@@ -350,8 +349,7 @@ export default function Home() {
                     </a>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                         <a className="ml-3 text-gray-400" target="_blank" rel="noreferrer" href="https://github.com/dtr-beast/Cosine-Similarity">
-                            <Image className="dark:text-gray-500" src="/github-light.png" width={48} height={48}
-                                   alt="Github Logo"/>
+                            <img src="/static/images/github-light.png" alt="GitHub Logo" width={48} height={48}/>
                         </a>
                     </span>
                 </div>
